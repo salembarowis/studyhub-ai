42f6ce51207a685406d05690c7bbfbb515617aac
ahmed <ah.attas@hotmail.com>
2026-05-28 09:10:42 +0300

    Added new features

---
+++ script.js
@@ -0,0 +1,12 @@
+const uploadButton = document.querySelector(".upload-section button");
+
+uploadButton.addEventListener("click", function(){
+
+    alert("File uploaded successfully!");
+
+});
+function toggleDarkMode(){
+
+    document.body.classList.toggle("dark");
+
+}
